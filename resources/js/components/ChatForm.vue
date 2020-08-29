61c56ac8725d0b6fe99f9fcf23850bd3c28a72cb
omaresmael <omaresmael@github.com>
2020-08-29 21:51:57 +0200

    simple laravel-vue chat function

---
+++ resources/js/components/ChatForm.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="input-group">
+        <input id="btn-input" type="text" name="message" class="form-control input-sm" placeholder="Type your message here..." v-model="newMessage" @keyup.enter="sendMessage">
+
+        <span class="input-group-btn" style="margin-top: 4px; margin-left: 4px">
+            <button class="btn btn-primary btn-sm" id="btn-chat" @click="sendMessage">
+                Send
+            </button>
+        </span>
+    </div>
+</template>
+
+<script>
+    export default {
+
+
+        props: ['user'],
+
+        data() {
+            return {
+                newMessage: ''
+            }
+        },
+
+        methods: {
+            sendMessage() {
+                this.$emit('messagesent', {
+                    user: this.user,
+                    message: this.newMessage
+                });
+
+                this.newMessage = ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,26 @@
+<!-- resources/views/chat.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container offset-md-3">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading offset-md-4 display-4">Chats</div>
+
+                    <div class="panel-body">
+                        <chat-messages :messages="messages"></chat-messages>
+                    </div>
+                    <div class="panel-footer">
+                        <chat-form
+                            @messagesent="addMessage"
+                            :user="{{ Auth::user() }}"
+                        ></chat-form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
